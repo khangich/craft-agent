5596ea9f634f035a4d42c6db3a66a7f19b40f234
Khang Pham <khangphama@gmail.com>
2024-06-04 21:23:20 -0700

    add token

--- githubtools.py
+++ githubtools.py
@@ -9,15 +9,15 @@ import requests
 class Config:
     def __init__(self):
         self.USERNAME = os.getenv('USERNAME')
-        self.GITHUB_API_TOKEN = os.getenv('GITHUB_API_TOKEN')
+        self.API_TOKEN = os.getenv('API_TOKEN')
         self.REPO = os.getenv('REPO')
 
     def validate(self):
-        if not all([self.USERNAME, self.GITHUB_API_TOKEN, self.REPO]):
-            raise ValueError("One or more environment variables are missing. Please ensure USERNAME, GITHUB_API_TOKEN, and REPO are set.")
+        if not all([self.USERNAME, self.API_TOKEN, self.REPO]):
+            raise ValueError("One or more environment variables are missing. Please ensure USERNAME, API_TOKEN, and REPO are set.")
 
 
-auth = Auth.Token(Config().GITHUB_API_TOKEN)
+auth = Auth.Token(Config().API_TOKEN)
 g = Github(auth=auth)
 if not g.get_user().login:
     print("not login")
@@ -38,7 +38,7 @@ def get_recent_pull_request():
 def get_pull_request_comment():
     headers = {
         "Accept": "application/vnd.github+json",
-        "Authorization": f"Bearer {Config().GITHUB_API_TOKEN}",
+        "Authorization": f"Bearer {Config().API_TOKEN}",
         "X-GitHub-Api-Version": "2022-11-28"
     }
     url = f"https://api.github.com/repos/{Config().REPO}/pulls/{get_recent_pull_request()}/comments"
